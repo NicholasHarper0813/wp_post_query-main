69ef971678028a586c3a46ab6552b627f142a0dc
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-11 06:04:01 -0800

    Update reducer.js

--- test/reducer.js
+++ test/reducer.js
@@ -1,15 +1,7 @@
-/**
- * External dependencies
- */
 import { expect } from 'chai';
 import deepFreeze from 'deep-freeze';
 import { keyBy } from 'lodash';
-
-/**
- * Internal dependencies
- */
 import {
-	// action-types
 	POST_REQUEST,
 	POST_REQUEST_FAILURE,
 	POST_REQUEST_SUCCESS,
@@ -17,7 +9,6 @@ import {
 	POSTS_REQUEST,
 	POSTS_REQUEST_FAILURE,
 	POSTS_REQUEST_SUCCESS,
-	// reducers
 	items,
 	requests,
 	totalPages,
@@ -46,36 +37,6 @@ describe( 'Post reducer', () => {
 			const originalState = deepFreeze( keyBy( posts, 'id' ) );
 			const newState = items( originalState, { type: POSTS_RECEIVE, posts: [ post ] } );
 			expect( newState ).to.eql( { ...originalState, 7: post } );
-		} );
-	} );
-
-	describe( 'queryRequests', () => {
-		it( 'should have no change by default', () => {
-			const newState = queryRequests( undefined, {} );
-			expect( newState ).to.eql( {} );
-		} );
-
-		it( 'should track the requesting state of new queries', () => {
-			const newState = queryRequests( undefined, { type: POSTS_REQUEST, query: { paged: 1 } } );
-			expect( newState ).to.eql( { '{"paged":1}': true } );
-		} );
-
-		it( 'should track the requesting state of successful queries', () => {
-			const originalState = deepFreeze( { '{"paged":1}': true } );
-			const newState = queryRequests( originalState, { type: POSTS_REQUEST_SUCCESS, query: { paged: 1 } } );
-			expect( newState ).to.eql( { '{"paged":1}': false } );
-		} );
-
-		it( 'should track the requesting state of failed queries', () => {
-			const originalState = deepFreeze( { '{"paged":1}': true } );
-			const newState = queryRequests( originalState, { type: POSTS_REQUEST_FAILURE, query: { paged: 1 } } );
-			expect( newState ).to.eql( { '{"paged":1}': false } );
-		} );
-
-		it( 'should track the requesting state of additional queries', () => {
-			const originalState = deepFreeze( { '{"paged":1}': false } );
-			const newState = queryRequests( originalState, { type: POSTS_REQUEST, query: { paged: 2 } } );
-			expect( newState ).to.eql( { ...originalState, '{"paged":2}': true } );
 		} );
 	} );
 
@@ -109,34 +70,33 @@ describe( 'Post reducer', () => {
 		} );
 	} );
 
-	describe( 'queries', () => {
+	describe( 'queryRequests', () => {
 		it( 'should have no change by default', () => {
-			const newState = queries( undefined, {} );
+			const newState = queryRequests( undefined, {} );
 			expect( newState ).to.eql( {} );
 		} );
 
-		it( 'should track the post IDs for requested queries', () => {
-			const action = {
-				type: POSTS_REQUEST_SUCCESS,
-				query: { paged: 1 },
-				posts
-			};
-			const newState = queries( undefined, action );
-			expect( newState ).to.eql( { '{"paged":1}': [ 6, 5, 4, 3 ] } );
+		it( 'should track the requesting state of new queries', () => {
+			const newState = queryRequests( undefined, { type: POSTS_REQUEST, query: { paged: 1 } } );
+			expect( newState ).to.eql( { '{"paged":1}': true } );
 		} );
 
-		it( 'should track the post IDs for additional requested queries', () => {
-			const originalState = deepFreeze( { '{"paged":1}': [ 6, 5, 4, 3 ] } );
-			const action = {
-				type: POSTS_REQUEST_SUCCESS,
-				query: { paged: 2 },
-				posts: [ post ]
-			};
-			const newState = queries( originalState, action );
-			expect( newState ).to.eql( {
-				'{"paged":1}': [ 6, 5, 4, 3 ],
-				'{"paged":2}': [ 7 ]
-			} );
+		it( 'should track the requesting state of successful queries', () => {
+			const originalState = deepFreeze( { '{"paged":1}': true } );
+			const newState = queryRequests( originalState, { type: POSTS_REQUEST_SUCCESS, query: { paged: 1 } } );
+			expect( newState ).to.eql( { '{"paged":1}': false } );
+		} );
+
+		it( 'should track the requesting state of failed queries', () => {
+			const originalState = deepFreeze( { '{"paged":1}': true } );
+			const newState = queryRequests( originalState, { type: POSTS_REQUEST_FAILURE, query: { paged: 1 } } );
+			expect( newState ).to.eql( { '{"paged":1}': false } );
+		} );
+
+		it( 'should track the requesting state of additional queries', () => {
+			const originalState = deepFreeze( { '{"paged":1}': false } );
+			const newState = queryRequests( originalState, { type: POSTS_REQUEST, query: { paged: 2 } } );
+			expect( newState ).to.eql( { ...originalState, '{"paged":2}': true } );
 		} );
 	} );
 
@@ -167,6 +127,37 @@ describe( 'Post reducer', () => {
 			expect( newState ).to.eql( {
 				'wordpress-query-component-tests': 6,
 				'a-single-post': 7
+			} );
+		} );
+	} );
+
+	describe( 'queries', () => {
+		it( 'should have no change by default', () => {
+			const newState = queries( undefined, {} );
+			expect( newState ).to.eql( {} );
+		} );
+
+		it( 'should track the post IDs for requested queries', () => {
+			const action = {
+				type: POSTS_REQUEST_SUCCESS,
+				query: { paged: 1 },
+				posts
+			};
+			const newState = queries( undefined, action );
+			expect( newState ).to.eql( { '{"paged":1}': [ 6, 5, 4, 3 ] } );
+		} );
+
+		it( 'should track the post IDs for additional requested queries', () => {
+			const originalState = deepFreeze( { '{"paged":1}': [ 6, 5, 4, 3 ] } );
+			const action = {
+				type: POSTS_REQUEST_SUCCESS,
+				query: { paged: 2 },
+				posts: [ post ]
+			};
+			const newState = queries( originalState, action );
+			expect( newState ).to.eql( {
+				'{"paged":1}': [ 6, 5, 4, 3 ],
+				'{"paged":2}': [ 7 ]
 			} );
 		} );
 	} );
